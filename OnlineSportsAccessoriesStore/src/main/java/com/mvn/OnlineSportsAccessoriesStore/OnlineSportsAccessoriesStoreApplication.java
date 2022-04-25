85770530b963519845f4c8f7bd187e44567c4ca1
Shaik-muzamil-03 <shaikmuzammilbasha03@gmail.com>
2022-04-25 13:07:11 +0530

    committed and pushed

---
+++ OnlineSportsAccessoriesStore/src/main/java/com/mvn/OnlineSportsAccessoriesStore/OnlineSportsAccessoriesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mvn.OnlineSportsAccessoriesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSportsAccessoriesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSportsAccessoriesStoreApplication.class, args);
+	}
+
+}
---
+++ OnlineSportsAccessoriesStore/src/test/java/com/mvn/OnlineSportsAccessoriesStore/OnlineSportsAccessoriesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvn.OnlineSportsAccessoriesStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSportsAccessoriesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
